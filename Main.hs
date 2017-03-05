bdc7938ab0b1844761b9cfacefa8a9ba163b976f
Remy Goldschmidt <taktoa@gmail.com>
2017-03-05 09:08:25 -0600

    save changes

--- Main.hs
+++ Main.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor       #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 {-@ LIQUID "--full" @-}
 module Main where
 
 import           GHC.Generics (Generic)
-
--- {-@ type NonEmpty = {v : [a] | 0 < length v } @-}
 
 -- | The type of generalized regular expressions over a given alphabet.
 data Regex tok
@@ -28,33 +27,108 @@ data Regex tok
   -- ^ The intersection of two regular expressions.
   deriving (Eq, Functor, Generic)
 
+{-@ autosize Regex @-}
+
+{-
+data Regex tok
+  = Ø
+  | Ɛ
+  | Tok   tok
+  | Comp  (Regex tok)
+  | Star  (Regex tok)
+  | (:|:) (Regex tok) (Regex tok)
+  | (:&:) (Regex tok) (Regex tok)
+  | (:.:) (Regex tok) (Regex tok)
+-}
+
+{-@ measure ratomic @-}
+ratomic :: Regex tok -> Bool
+ratomic Ø           = True
+ratomic Ɛ           = True
+ratomic (Tok t)     = True
+ratomic (Star r)    = False
+ratomic (Comp r)    = False
+ratomic ((:.:) r s) = False
+ratomic ((:|:) r s) = False
+ratomic ((:&:) r s) = False
+
+
+{-@ inline maxInt @-}
+maxInt :: Int -> Int -> Int
+maxInt x y = if x > y then x else y
+
+-- -- rsize :: rx:(Regex tok) -> {v : Int | (v >= 0) && (not (ratomic rx) => (v > 0))}
+-- {-@ measure rsize @-}
+-- {-@ rsize :: rx:(Regex tok) -> {v : Int | (v >= 0) && (v = rsize rx)} @-}
+-- rsize :: Regex tok -> Int
+-- rsize Ø           = 0
+-- rsize Ɛ           = 0
+-- rsize (Tok _)     = 0
+-- rsize (Star r)    = 1 + rsize r
+-- rsize (Comp r)    = 1 + rsize r
+-- rsize ((:.:) r s) = 1 + maxInt (rsize r) (rsize s)
+-- rsize ((:|:) r s) = 1 + maxInt (rsize r) (rsize s)
+-- rsize ((:&:) r s) = 1 + maxInt (rsize r) (rsize s)
+
+{-@ type RegexN tok n = {rx : Regex tok | (n >= rsize rx)} @-}
+
+{-@ measure isNullable @-}
+-- | Returns true if the language of the given regular expression contains Ɛ,
+--   and otherwise returns false.
+isNullable :: Regex tok -> Bool
+isNullable Ø           = False
+isNullable Ɛ           = True
+isNullable (Tok _)     = False
+isNullable (Comp r)    = not (isNullable r)
+isNullable (Star r)    = True
+isNullable ((:|:) r s) = isNullable r || isNullable s
+isNullable ((:&:) r s) = isNullable r && isNullable s
+isNullable ((:.:) r s) = isNullable r && isNullable s
+
 -- | Returns Ɛ if the language of the given regular expression contains Ɛ, and
 --   otherwise returns Ø.
 nullable :: Regex tok -> Regex tok
-nullable = \x -> if go x then Ɛ else Ø
-  where
-    go :: Regex t -> Bool
-    go Ø         = False
-    go Ɛ         = True
-    go (Tok _)   = False
-    go (Comp r)  = not (go r)
-    go (Star _)  = True
-    go (r :|: s) = go r || go s
-    go (r :&: s) = go r && go s
-    go (r :.: s) = go r && go s
+nullable r = if isNullable r then Ɛ else Ø
 
--- | The Brzozowski derivative of a regular expression with respect to a token.
+{-@ axiomatize isEmpty @-}
+isEmpty Ø = True
+isEmpty r = False
+
 deriv :: (Eq tok) => tok -> Regex tok -> Regex tok
-deriv _ Ø         = Ø
-deriv _ Ɛ         = Ø
+deriv a Ø         = Ø
+deriv a Ɛ         = Ø
 deriv a (Tok t)   = if t == a then Ɛ else Ø
 deriv a (Comp r)  = Comp (deriv a r)
 deriv a (Star r)  = deriv a r :.: Star r
 deriv a (r :|: s) = deriv a r :|: deriv a s
 deriv a (r :&: s) = deriv a r :&: deriv a s
-deriv a (r :.: s) = let r' = deriv a r
-                        s' = deriv a s
-                    in (r' :.: s) :|: (nullable r :.: s')
+deriv a (r :.: s) = let d1 = deriv a r
+                        d2 = deriv a s
+                    in if isNullable r
+                       then (d1 :.: s) :|: d2
+                       else d1 :.: s
+
+
+-- {-@ deriv' :: n:Nat -> tok -> RegexN tok n -> Regex tok @-}
+-- deriv' :: (Eq tok) => Int -> tok -> Regex tok -> Regex tok
+-- deriv' n a = if n > 0 then go n a else id
+--   where
+--     go n a Ø         = Ø
+--     go n a Ɛ         = Ø
+--     go n a (Tok t)   = if t == a then Ɛ else Ø
+--     go n a (Comp r)  = Comp (deriv' (n - 1) a r)
+--     go n a (Star r)  = deriv' (n - 1) a r :.: Star r
+--     go n a (r :|: s) = deriv' (n - 1) a r :|: deriv' (n - 1) a s
+--     go n a (r :&: s) = deriv' (n - 1) a r :&: deriv' (n - 1) a s
+--     go n a (r :.: s) = let d1 = deriv' (n - 1) a r
+--                            d2 = deriv' (n - 1) a s
+--                        in if isNullable r
+--                           then (d1 :.: s) :|: d2
+--                           else d1 :.: s
+--
+-- -- | The Brzozowski derivative of a regular expression with respect to a token.
+-- deriv :: (Eq tok) => tok -> Regex tok -> Regex tok
+-- deriv a rx = deriv' (succ (rsize rx)) a rx
 
 -- | The Brzozowski derivative of a regular expression with respect to a string.
 derivative :: (Eq tok, Foldable t) => t tok -> Regex tok -> Regex tok
