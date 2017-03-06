950c6a6646304b6acaa81bdb2bf9bb4743637fd5
Remy Goldschmidt <taktoa@gmail.com>
2017-03-06 02:18:54 -0600

    saving all changes

--- Main.hs
+++ Main.hs
@@ -2,10 +2,11 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-{-@ LIQUID "--fullcheck" @-}
+{- LIQUID "--fullcheck" -}
 module Main where
 
 import           GHC.Generics (Generic)
+-- import           ProofCombinators
 
 -- | The type of generalized regular expressions over a given alphabet.
 data Regex tok
@@ -46,6 +47,38 @@ isNullable ((:.:) r s) = isNullable r && isNullable s
 nullable :: Regex tok -> Regex tok
 nullable r = if isNullable r then Ɛ else Ø
 
+{-@ type Fin n a = {xs : [a] | ((len xs) <= k)} @-}
+
+{-@ assume splitAt
+    :: forall a. k:Nat
+    -> xs:(Fin k a)
+    -> {p : ([a], [a]) | (k = (len (fst p)))
+                         && ((len xs) = (len (fst p) + len (snd p)))} @-}
+
+{- inline myAny -}
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ []     = False
+myAny f (x:xs) = f x || myAny f xs
+
+-- {- range :: lower:Int -> upper:Int -> [{x : Int | (lower <= x) && (x <= upper)}] -}
+-- range :: Int -> Int -> [Int]
+-- range = \lower upper -> reverse (go lower upper)
+--   where
+--     {- go :: lower:Int -> upper:Int -> [{x : Int | (lower <= x) && (x <= upper)}] / [upper] -}
+--     go :: Int -> Int -> [Int]
+--     go lower upper = if lower <= upper then [] else upper : go lower (upper - 1)
+
+-- {- axiomatize matches -}
+-- {- matches :: [tok] -> rx:(Regex tok) -> Bool / [autolen rx] -}
+-- matches :: (Eq tok) => [tok] -> Regex tok -> Bool
+-- matches []  Ɛ         = True
+-- matches [a] (Tok tok) = tok == a
+-- matches str (r :|: s) = matches str r || matches str s
+-- matches str (r :&: s) = matches str r && matches str s
+-- matches str (r :.: s) = myAny (\(a, b) -> matches a r && matches b s)
+--                         $ map (\i -> splitAt i str) [0 .. length str]
+-- matches _   _         = False
+
 -- | The Brzozowski derivative of a regular expression with respect to a token.
 deriv :: (Eq tok) => tok -> Regex tok -> Regex tok
 deriv a Ø         = Ø
@@ -64,6 +97,16 @@ deriv a (r :.: s) = let d1 = deriv a r
 -- | The Brzozowski derivative of a regular expression with respect to a string.
 derivative :: (Eq tok, Foldable t) => t tok -> Regex tok -> Regex tok
 derivative tokens rx = foldr deriv rx tokens
+
+{-
+derivWorks
+  :: x:tok
+  -> xs:[tok]
+  -> rx:(Regex tok)
+  -> {v : Proof | (Prop (matches (x:xs) rx)) <=> (Prop (matches xs (deriv x rx))) }
+-}
+-- derivWorks :: tok -> [tok] -> Regex tok -> ()
+-- derivWorks x xs rx = trivial *** QED
 
 main :: IO ()
 main = pure ()
